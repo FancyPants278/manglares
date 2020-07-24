1a47a2203325f333702aa6c07aac8522a08172d7
Fabbiologia <favoretto.fabio@gmail.com>
2020-07-23 22:14:54 -0600

    added a new script only with code for visualization

---
+++ 02_data_visualization.R
@@ -0,0 +1,82 @@
+library(tidyverse)
+library(lubridate)
+
+source("01_data_wrangling.R")
+
+# plotting ----------------------------------------------------------------
+
+# setting default theme
+theme_set(theme_linedraw())
+
+
+# NDVI --------------------------------------------------------------------
+
+
+(p1 <- ggplot(ndvi, aes(x=date, y=var, col=region))+
+         geom_line(cex=.4)+
+         labs(x="", y="NDVI")+
+         geom_hline(aes(yintercept=0), col = "black")+
+         geom_smooth(col="red")+
+         facet_wrap(~region)+
+         scale_color_brewer(palette = 2, aesthetics="black")+
+         theme(legend.position = "", 
+               panel.grid = element_blank()))
+
+(p2 <- ndvi %>% 
+                mutate(month = format(date, "%m"), year = format(date, "%Y")) %>% 
+                group_by(region, month) %>% 
+                mutate(average = mean(var, na.rm = T), anomaly = var-average) %>% 
+                ggplot(aes(x=date, y=anomaly)) +
+                geom_col(aes(fill= anomaly>0))+
+                geom_hline(yintercept = 0)+
+                ylim(-1,.9)+
+                scale_fill_manual(labels = c("FALSE" = "Lower than avg", "TRUE"="Higher than avg"), 
+                                  values = c("blue", "red"))+
+                labs(x ="", y = "NDVI anomaly")+
+                facet_wrap(~region)+
+                theme_linedraw()+
+                theme(legend.title = element_blank(), 
+                      panel.grid = element_blank()))
+
+
+
+
+# Temperature -------------------------------------------------------------
+
+
+(p1 <- ggplot(temp, aes(x=date, y=var, col=region))+
+         geom_line(cex=.4)+
+         labs(x="", y="Temperature (°C)")+
+         ylim(5,60)+
+         geom_hline(aes(yintercept=32.5), col = "black")+
+         geom_smooth(col="red")+
+         facet_wrap(~region)+
+         scale_color_brewer(palette = 2, aesthetics="black")+
+         theme_linedraw()+
+         theme(legend.position = "", 
+               panel.grid = element_blank()))
+
+
+
+## CUIDADO ESTO VA A SER LENTO! p.s. estas anomalias se ven raras...
+(p2 <- temp %>% 
+                mutate(month = format(date, "%m"), year = format(date, "%Y")) %>% 
+                group_by(region, month) %>% 
+                mutate(average = mean(var, na.rm = TRUE), anomaly = var-average) %>% 
+                ggplot(aes(x=date, y=anomaly)) +
+                geom_col(aes(fill= anomaly>0))+
+                scale_fill_manual(labels = c("FALSE" = "Colder than avg", "TRUE"="Warmer than avg"), 
+                                  values = c("blue", "red"))+
+                labs(x ="", y = "Temperature anomaly °C")+
+                facet_wrap(~region)+
+                theme_linedraw()+
+                theme(legend.title = element_blank(), 
+                      panel.grid = element_blank()))
+
+
+
+## CUIDADO!! ESTO ABAJO VA A SER MUY LENTO!!! si quieres intentar quita el comento #
+# p1/p2
+
+
+
